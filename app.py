320c69ea2ef585ee0435f1a1faeb33c238418fc6
Amit Kumar <70321164+amitprna@users.noreply.github.com>
2022-01-18 16:21:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import numpy as np
 import requests
 from streamlit_lottie import st_lottie
 
+st.set_page_config(page_title="Emotion Detection", page_icon="🐱‍🚀")
+
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
