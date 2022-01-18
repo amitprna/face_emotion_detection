d0ebe8e87260e9eab034d1a6f37251cccb5a17d0
Amit Kumar <70321164+amitprna@users.noreply.github.com>
2022-01-18 16:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ with two:
         st.write('**No image for processing**')
         url2 = "https://assets10.lottiefiles.com/private_files/lf30_jo7huq2d.json"
         res_json = load_lottieurl(url2)
-        st_lottie(res_json,height=70,width=70)
+        st_lottie(res_json,height=90,width=90)
